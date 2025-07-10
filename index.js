eb97c612bf2ad673875900ba5502d93ec06c3f3c
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-07-10 15:57:55 +0600

    added-upload-user

--- index.js
+++ index.js
@@ -3,10 +3,60 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@my-user.d2otqer.mongodb.net/?retryWrites=true&w=majority&appName=my-user`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // --------Db Collection start ---------//
+    const db = client.db('Study-DB');
+    const userCollections = db.collection('users');
+    // --------Db Collection end ---------//
+
+
+    //------- upload user Data in Db -------//
+    app.post('/users', async(req, res) => {
+      const email = req.body.email;
+      const existingUser = await userCollections.findOne({email});
+      if(existingUser){
+        return res.status(200).send({message: 'User already exists'})
+      };
+
+      const user = req.body;
+      const result = await userCollections.insertOne(user);
+      res.send(result);
+    })
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
